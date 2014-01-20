ccc73a46491fdbfe28801206018bcee87a42f517
James English <jamestenglish@gmail.com>
2014-01-20 10:34:49 -0600

    added index and tweaked output

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+   Console energy data visualization
+ * data-vis
+ * https://github.com/jamesenglish/node-data-vis
+ *
+ * Copyright (c) 2014 James English
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+require("lib/data-vis");
